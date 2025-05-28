77bbea2b28d872bd5c06b10680141e4cc5b4f89e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-28 17:19:41 -0300

    Arquivo decrypt refatorado
    - Objecto de descriptografia congelado.
    - Código formatado.
    - Função decryptText exportada diretamente (gerará erro).

--- src/js/decrypt.js
+++ src/js/decrypt.js
@@ -1,15 +1,18 @@
 import { auxiliary } from "./auxiliary.js";
 
-const decryption = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
+const DECRYPTION = Object.freeze([
+  ["enter", "e"],
+  ["imes", "i"],
+  ["ai", "a"],
+  ["ober", "o"],
+  ["ufat", "u"],
+]);
 
-function decryptText(input, output, decryption) {
-    let text = auxiliary.formatText(input.value);
+export function decryptText(input, output, decryption = DECRYPTION) {
+  let text = auxiliary.formatText(input.value);
 
-    for(let code of decryption) { text = text.replaceAll(code[0], code[1]); }
-    output.value = text;
-}
-
-export const decrypt = {
-    decryption,
-    decryptText
+  for (let code of decryption) {
+    text = text.replaceAll(code[0], code[1]);
+  }
+  output.value = text;
 }
