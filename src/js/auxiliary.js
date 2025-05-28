dd6960b65844e30d09cce4d49065e274c4510453
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-28 15:50:02 -0300

    Formatação do código

--- src/js/auxiliary.js
+++ src/js/auxiliary.js
@@ -1,36 +1,54 @@
-function formatText(text) { return text.trim().toLowerCase(); }
+function formatText(text) {
+  return text.trim().toLowerCase();
+}
 
-function copyTextToClipboard(text) { navigator.clipboard.writeText(text); }
+function copyTextToClipboard(text) {
+  navigator.clipboard.writeText(text);
+}
 
-function hideElement(element) { element.classList.add("invisible"); }
+function hideElement(element) {
+  element.classList.add("invisible");
+}
 
-function showElement(element) { element.classList.remove("invisible"); }
+function showElement(element) {
+  element.classList.remove("invisible");
+}
 
 function animateButtonState(button, newText) {
-    const defaultText = button.textContent;
-    button.textContent = newText;
-    button.animate([
-        { borderColor: "green", color: "green", pointerEvents: "none", offset: 0 },
-        { color: "transparent", pointerEvents: "none", offset: 0.75 }
-    ], 1800);
-    setTimeout(() => { button.textContent = defaultText }, 1200);
+  const defaultText = button.textContent;
+  button.textContent = newText;
+  button.animate(
+    [
+      {
+        borderColor: "green",
+        color: "green",
+        pointerEvents: "none",
+        offset: 0,
+      },
+      { color: "transparent", pointerEvents: "none", offset: 0.75 },
+    ],
+    1800
+  );
+  setTimeout(() => {
+    button.textContent = defaultText;
+  }, 1200);
 }
 
 function outputMessage(input, containers) {
-    if(input.value.length > 0) {
-        hideElement(containers.message);
-        showElement(containers.output);
-        return;
-    }
-    showElement(containers.message);
-    hideElement(containers.output);
+  if (input.value.length > 0) {
+    hideElement(containers.message);
+    showElement(containers.output);
+    return;
+  }
+  showElement(containers.message);
+  hideElement(containers.output);
 }
 
 export const auxiliary = {
-    formatText,
-    copyTextToClipboard,
-    hideElement,
-    showElement,
-    animateButtonState,
-    outputMessage
-}
+  formatText,
+  copyTextToClipboard,
+  hideElement,
+  showElement,
+  animateButtonState,
+  outputMessage,
+};
