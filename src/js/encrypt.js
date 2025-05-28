d601ead9235f8bc6b12c28fd0b5c253f91680fa2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-05-28 17:15:29 -0300

    Arquivo encrypt refatorado
    - Código formatado.
    - Objeto de criptografia congelado.
    - A função encryptText é exportada diretamente (gerará erro).

--- src/js/encrypt.js
+++ src/js/encrypt.js
@@ -1,15 +1,18 @@
 import { auxiliary } from "./auxiliary.js";
 
-const encryption = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+const ENCRYPTION = Object.freeze([
+  ["e", "enter"],
+  ["i", "imes"],
+  ["a", "ai"],
+  ["o", "ober"],
+  ["u", "ufat"],
+]);
 
-function encryptText(input, output, encryption) {
-    let text = auxiliary.formatText(input.value);
+export function encryptText(input, output, encryption = ENCRYPTION) {
+  let text = auxiliary.formatText(input.value);
 
-    for(let code of encryption) { text = text.replaceAll(code[0], code[1]); }
-    output.value = text;
-}
-
-export const encrypt = {
-    encryption,
-    encryptText
+  for (let code of encryption) {
+    text = text.replaceAll(code[0], code[1]);
+  }
+  output.value = text;
 }
